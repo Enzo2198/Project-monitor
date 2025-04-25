802e528514f10f7d66796ee8be25457e75477832
vgspc <gianggvf8209@fullstack.edu.vn>
2025-04-25 16:19:12 +0700

    submit homeworks

---
+++ web/src/main.tsx
@@ -0,0 +1,32 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Product from './pages/Product'
+import Employee from './pages/Employee'
+// import App from './App.tsx'
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/hello",
+        element: <div>Hello World</div>,
+    },
+    {
+        path: "/product",
+        element: <Product/>,
+    },
+    {
+        path: "/employee",
+        element: <Employee/>,
+    },
+]);
+
+const root = document.getElementById("root");
+
+createRoot(root!).render(
+    <RouterProvider router={router} />
+)
