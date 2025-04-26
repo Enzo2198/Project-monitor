387c62dbaa3a85113a701b474e5ada6ef7a18525
vgspc <gianggvf8209@fullstack.edu.vn>
2025-04-26 13:48:50 +0700

    submit homeworks

--- web/src/pages/Employee/index.tsx
+++ web/src/pages/Employee/index.tsx
@@ -1,8 +1,17 @@
-import {FTable} from "../../components";
-import {Header, Employee} from "../../utils";
-
+import { useState } from 'react';
+import {EmployeeDialog, FTable} from '../../components'
+import {Header, Employee} from '../../utils'
+import {Button} from "@mui/material";
 
 export default () => {
+  const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false);
+  const [curEmployee, setCurEmployee] = useState<Employee>({
+    id: null,
+    name: '',
+    age: '',
+    address: '',
+  })
+
   const headers: Header[] = [
     {name: 'id', text: 'ID'},
     {name: 'name', text: 'Ten'},
@@ -11,14 +20,45 @@ export default () => {
     {name: 'action', text: ''},
   ]
 
-  const employees: Employee[] = [
+  const [employees, setEmployee] = useState<Employee[]>([
     {id: 1, name: 'Dung', age: 20, address: 'Thanh Oai - Ha Noi'},
     {id: 2, name: 'Trung', age: 22, address: 'Quoc Oai - Ha Noi'},
     {id: 3, name: 'Son', age: 30, address: 'Thanh Oai 2 - Ha Noi'},
-  ]
+  ])
+
+  const onAdd = () => {
+    setIsOpenDialog(true);
+  }
+
+  const onUpdate = (id: number) => {
+    console.log(id)
+    // @ts-ignore
+    setCurEmployee({...employees.find(e => e.id === id)})
+    setIsOpenDialog(true)
+  }
+
+  const onSave = () => {
+    setEmployee([...employees, curEmployee])
+    setIsOpenDialog(false);
+  }
+
   return (
     <>
-      <FTable tableName={'Employee'} headers={headers} rows={employees}/>
+      <h1>Employee</h1>
+      <Button variant="outlined" onClick={onAdd}>Add</Button>
+      <FTable
+        tableName={'employee hihi'}
+        headers={headers}
+        rows={employees}
+        onUpdate={onUpdate}
+      />
+      <EmployeeDialog
+        employee={curEmployee}
+        setEmployee={setCurEmployee}
+        onSave={onSave}
+        isOpen={isOpenDialog}
+        onClose={() => setIsOpenDialog(false)}
+      />
     </>
   )
 }
--- web/src/pages/Product/index.tsx
+++ web/src/pages/Product/index.tsx
@@ -1,71 +1,40 @@
-import {FTable, DialogContainer} from "../../components";
-import {Header, Product} from "../../utils";
-import Button from '@mui/material/Button';
+import {FTable, ProductDialog} from '../../components'
+import {Header, Product} from '../../utils'
+import {Button} from "@mui/material";
 import {useState} from "react";
 
-
 export default () => {
-  const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false);
-  const [selectProduct, setSelectProduct] = useState<Product | undefined>();
+  const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false)
 
   const headers: Header[] = [
-    {name: 'id', text: 'ID'},
-    {name: 'name', text: 'Ten'},
-    {name: 'type', text: 'Kieu'},
-    {name: 'original', text: 'Xuat xu'},
-    {name: 'action', text: ''},
+    { name: 'id', text: 'ID' },
+    { name: 'name', text: 'Ten' },
+    { name: 'type', text: 'Kieu' },
+    { name: 'original', text: 'Xuat xu' },
+    { name: 'action', text: '' }
   ]
 
-  const [products, setProducts]  = useState<Product[]> ([
-    {id: 1, name: 'Hang 1', type: '1', original: 'Trung Quoc'}
+  const [products, setProducts] = useState<Product[]>([
+    { id: 1, name: 'Hang 1', type: '1',  original:'Trung quoc' },
   ])
+
   const onAdd = () => {
-    setIsOpenDialog(true);
-    setSelectProduct(undefined);
+    setIsOpenDialog(true)
   }
 
-  const onSave = (data: Product) => {
-    if (selectProduct) {
-      setProducts(products.map(prod => prod.id === selectProduct.id? {
-        ...selectProduct, ...data} : prod
-      ));
-    } else {
-      const newProduct = {
-        id: products.length + 1,
-        name: data.name,
-        type: data.type,
-        original: data.original,
-      }
-      setProducts([...products, newProduct]);
-    }
-    setIsOpenDialog(false);
-    setSelectProduct(undefined);
-  }
-
-  const onEdit =(product: Product) => {
-    setSelectProduct(product);
-    setIsOpenDialog(true);
-  }
-
-    return (
-        <>
-          <Button variant="outlined" onClick={onAdd}>Add</Button>
-          <FTable
-            tableName={'Product'}
-            headers={headers}
-            rows={products}
-            onEdit={onEdit}
-          />
-          <DialogContainer
-            isOpen={isOpenDialog}
-            onClose={() => {
-              setIsOpenDialog(false)
-              setSelectProduct(undefined);
-            }}
-            onSave={onSave}
-            initialData={selectProduct}
-            products={products}
-          />
-        </>
-    )
+  return (
+    <>
+      <h1>Product</h1>
+      <Button variant="outlined" onClick={onAdd}>Add</Button>
+      <FTable
+        tableName={'product'}
+        headers={headers}
+        rows={products}
+      />
+      <ProductDialog
+        isOpen={isOpenDialog}
+        onClose={() => setIsOpenDialog(false)}
+      />
+    </>
+  )
 }
